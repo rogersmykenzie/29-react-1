da809450e253bc1d4f6869e8bb3efa128ae37924
Mykenzie <Work@Mykenzies-MacBook-Pro.local>
2020-03-09 14:38:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FuncComponent from "./components/FuncComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       firstName: "Erica"
+//     }
+//     // this.updateName = this.updateName.bind(this);
+//   }
+
+//   updateName = () => {
+//     // this.state.firstName = "Eric"; // NO NO
+//     this.setState({
+//       firstName: "Eric"
+//     });
+//   }
+
+//   render() {
+
+//     return (
+//       <div>
+//         <FuncComponent></FuncComponent>
+//         <FuncComponent></FuncComponent>
+//         <FuncComponent></FuncComponent>
+//         <FuncComponent></FuncComponent>
+//         <h1>{this.state.firstName}</h1>
+//         <h2>Rogers</h2>
+//         <button onClick={this.updateName}>Change Name!</button>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      userInput: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    // console.log(e.target.value);
+    this.setState({userInput: e.target.value})
+  }
+
+  handleClick = () => {
+    //Method 1
+    // let copy = this.state.todos.slice();
+    // copy.push(this.state.userInput);
+    // this.setState({todos: copy})
+    //Method 2
+    this.setState({todos: [...this.state.todos, <li>{this.state.userInput}</li>]})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Todos!</h1>
+        <input onChange={this.handleChange}  />
+        <button onClick={this.handleClick}>Add To List!</button>
+        <ul>{this.state.todos}</ul>
+      </div>
+    )
+  }
 }
 
+
+
+
+
+
 export default App;
+
+// class Athlete {
+//   constructor(name) {
+//     this.name = {
+//       firstName: "",
+//       lastName: ""
+//     };
+//   }
+
+//   sayName() {
+//     console.log(this.name.firstName)
+//   }
+// }
---
+++ src/components/FuncComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FuncComponent() {
+  return (
+    <div>
+      <h1>I am a functional component!</h1>
+    </div>
+  )
+}
+
+export default FuncComponent;
